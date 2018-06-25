a77a3dc0752eea2cf58edcad7d9942f2dd103822
Levi <lyderen@gmail.com>
2018-06-25 22:52:47 +0300

    envo fix

--- EnvaiermantVariable/setting.js
+++ EnvaiermantVariable/setting.js
@@ -2,8 +2,7 @@
 
 module.exports = {
     keys:{
-
-        urlAdmin: "/oiergewtu34t8urtd56yh58ugh3q9tu2u3428g3g31iko6bg40,975ednplloikkkju",
+        urlAdmin: "/oiergewtu34t80oipltd56yh58ugh3q9tu2u3428g3g31iko6bg40,975ednplloikkkju",
         tokenKey: "A-Z-B-Y-C-X-D-W-E-VFV"
     }
 }
